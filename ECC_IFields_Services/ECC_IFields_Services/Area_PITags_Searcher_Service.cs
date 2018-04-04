64b814d9434c4471ccab8750eef5d9467af72dfe
Houssam AlMoughrabi <houssam@git.cmeoffshore.com>
2018-04-04 10:30:59 +0300

    ECC-0: Create WS, DataLayer, PIAF services projects

---
+++ ECC_IFields_Services/ECC_IFields_Services/Area_PITags_Searcher_Service.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECC_IFields_Services
+{
+    partial class Area_PITags_Searcher_Service : ServiceBase
+    {
+        public Area_PITags_Searcher_Service()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // TODO: Add code here to start your service.
+        }
+
+        protected override void OnStop()
+        {
+            // TODO: Add code here to perform any tear-down necessary to stop your service.
+        }
+    }
+}
