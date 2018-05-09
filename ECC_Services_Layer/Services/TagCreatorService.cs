34a54a50952b07873ef6648306b5cd32d08214a4
Houssam AlMoughrabi <houssam@git.cmeoffshore.com>
2018-05-09 17:25:17 +0300

    ECC-0: Code cleaning TagCreatorService.cs

--- ECC_Services_Layer/Services/TagCreatorService.cs
+++ ECC_Services_Layer/Services/TagCreatorService.cs
@@ -22,25 +22,29 @@ namespace ECC_AFServices_Layer.Services
         private PIServer piServer;
         private List<PointSourceDataModel> _areasPointSources = new List<PointSourceDataModel>();
 
+        /// <summary>
+        /// Get all the non-created tags from oracle database and create them in the central PI Server:
+        /// </summary>
+        /// <returns></returns>
         public async Task<bool> StartAsync()
         {
             piServer = PIAFUtils.GetPIServer(_eccPIServerName);
             try
             {
+                // Get the Non-created tags from oracle database
                 var tags = await _tagCreatorStore.GetTagsForCreation();
+
                 if (tags != null && tags.Count() > 0)
                 {
-                    //PIServer piServer = PIAFUtils.GetPIServer(_eccPIServerName);
-                    //Collect all the tags basic attributes required for insertion
-                    //var _pointsDefinitions = tags.MapToPIPointDefinition(withDescriptor: true);
-                    //TODOto be implementedAdd the pointsrouce, location1 and location4 attributes gotten from ECCPI_POINT_SOURCES table (search for point source having the least number of tags)
                     try
                     {
+                        // Distinct the repeated tag names to avoid errors when creating the tags in PI server
                         tags = tags.DistinctBy(t => t.ECCPI_TAG_NAME);
 
+                        // Check and handle the tags before creation
                         await CheckForExistingTagsAsync(tags);
 
-                        //Insert the tags in bulk
+                        // Create the tags in central PI server
                         await CreateTagsAsync(tags);
                     }
                     catch (Exception e)
@@ -57,16 +61,19 @@ namespace ECC_AFServices_Layer.Services
             }
         }
 
+        /// <summary>
+        /// Create the valid tags in central PI server
+        /// </summary>
+        /// <param name="tags"></param>
+        /// <returns></returns>
         private async Task CreateTagsAsync(IEnumerable<PITagDataModel> tags)
         {
             //Initialy filter the list and screen the already created tags
             tags = tags.Where(t => t.ECCPI_TAG_CRE_FLG != "Y");
 
+            // Create the PI Points in PI Server
             var _pointsDefinitions = await GetPointsDefinitions(tags);
-
-            AFListResults<string, PIPoint> _insertResult = piServer.CreatePIPoints(_pointsDefinitions);
-
-            // TODO: Update the duplicates occurs before the insertion
+            AFListResults<string, PIPoint> _insertResult = piServer.CreatePIPoints(_pointsDefinitions);            
 
             //Receive results, and update the status of each inserted tag
             if (_insertResult != null && _insertResult.Results != null && _insertResult.Results.Count() > 0)
@@ -79,7 +86,7 @@ namespace ECC_AFServices_Layer.Services
                     successTags = successTags.Where(st => !_insertResult.Errors.Select(e => e.Key).Contains(st.ECCPI_TAG_NAME));
                 }
 
-                //Fill ECCPI_POINT_ID in successfull tags to be updating in oracle db
+                // Fill ECCPI_POINT_ID in successfull tags to be updating in oracle db
                 successTags.ForEach(st =>
                 {
                     st.ECCPI_POINT_ID = _successResults.Where(sr => sr.Name == st.ECCPI_TAG_NAME).FirstOrDefault().ID;
@@ -92,7 +99,7 @@ namespace ECC_AFServices_Layer.Services
                 Logger.Info(ServiceName, string.Format("{0} Inserted Tags", (successTags != null) ? successTags.Count() : 0));
                 await _tagCreatorStore.Commit();
 
-                //TODO: Update No of Tags
+                // Update the number of tags created in the area point source table
                 if (_areasPointSources != null && _areasPointSources.Count() > 0)
                 {
                     foreach (var ptSrc in _areasPointSources)
@@ -140,10 +147,14 @@ namespace ECC_AFServices_Layer.Services
             }
         }
 
-        ///TODO: to be implemented -- Search for instrument tags if they exist in the ECCPI
-        /// for existing tags => check if these are flagged to be renamed 
-        /// => if yes, rename the tag in ECCPI Server and update the existing tag name in oracle accordinally
-        /// => else, update the existing tag name field in oracle database
+
+        /// <summary>
+        /// Search for instrument tags if they exist in the ECCPI
+        ///     if yes => rename the tag in ECCPI Server and update the existing tag name in oracle accordinally
+        ///     else => update the existing tag name field in oracle database
+        /// </summary>
+        /// <param name="tags">List of tags to be checked and validated</param>
+        /// <returns></returns>
         private async Task CheckForExistingTagsAsync(IEnumerable<PITagDataModel> tags)
         {
             int _foundTagsNum = 0;
@@ -153,6 +164,7 @@ namespace ECC_AFServices_Layer.Services
             {
                 var _iTags = tags.Skip(_start).Take(_limit);
 
+                // Construct the query to search the PI server by instrutment tag name
                 IList<IEnumerable<PIPointQuery>> _queries = new List<IEnumerable<PIPointQuery>>();
                 foreach (var tag in _iTags)
                 {
@@ -161,28 +173,37 @@ namespace ECC_AFServices_Layer.Services
                     _queries.Add(_query);
                 }
 
+                // Execute the search query
                 var findExistingTags = await PIPoint.FindPIPointsAsync(piServer, queries: _queries, attributeNames: new List<string>() { PICommonPointAttributes.InstrumentTag });
+
+                // Itterate the result
                 foreach (var foundTag in findExistingTags)
                 {
+                    // Find the tag with matching instrumenttag name
                     var originalTag = tags.Where(t => t.AREA_PI_TAG_NAME == foundTag.GetAttribute(PICommonPointAttributes.InstrumentTag).ToString()).FirstOrDefault();
+
+                    // The result is valid and the exsiting tag name different than the found one
                     if (originalTag != null && originalTag.ECCPI_EXST_TAG_NAME != foundTag.Name)
                     {
                         if (!string.IsNullOrEmpty(originalTag.ECCPI_TAG_REN_RQST_FLG))
                         {
                             switch (originalTag.ECCPI_TAG_REN_RQST_FLG)
                             {
+                                // Tag isn't requested to be renamed => update the local databse with the existing tag name
                                 case "N":
                                 default:
                                     originalTag.ECCPI_EXST_TAG_NAME = foundTag.Name;
-                                    //update the record in oracle db
+                                    // Update the record in oracle db
                                     var updateExistingTag = await _tagCreatorStore.UpdateExistingTag(originalTag.EAWFT_NUM, foundTag.Name);
                                     originalTag.ECCPI_TAG_CRE_FLG = "Y";
                                     break;
+                                // Tag is requested to be renamed => Update the tag name in the central PI Server
                                 case "Y":
-                                    //Rename the tag in PI and update existing tag name accordinaly in oracle 
+                                    // Rename the tag in PI and update existing tag name accordinaly in oracle 
                                     foundTag.SetAttribute(PICommonPointAttributes.Tag, originalTag.ECCPI_TAG_NAME);
                                     foundTag.SaveAttributes(new string[] { PICommonPointAttributes.Tag });
 
+                                    // Flag the tag as renamed in oracle database
                                     var updateRenamedExistingTag = await _tagCreatorStore.UpdateExistingTag(originalTag.EAWFT_NUM, originalTag.ECCPI_TAG_NAME, renamedInPIServerFlag: true);
                                     originalTag.ECCPI_TAG_CRE_FLG = "Y";
                                     break;
@@ -197,96 +218,30 @@ namespace ECC_AFServices_Layer.Services
 
 
 
+        /// <summary>
+        /// Remap the tags into points definition
+        /// </summary>
+        /// <param name="tags"></param>
+        /// <returns></returns>
         public async Task<IDictionary<string, IDictionary<string, object>>> GetPointsDefinitions(IEnumerable<PITagDataModel> tags)
         {
 
+            // Group the tags list by source server name
             IEnumerable<string> tagsSourceServersNames = tags.GroupBy(t => t.SRC_PI_SERVER_CD).Select(t => t.Key);
             IDictionary<string, PointSourceDataModel> pointSources = new Dictionary<string, PointSourceDataModel>();
             foreach (var serverKey in tagsSourceServersNames)
             {
-                var availablePointSource = await _tagCreatorStore.GetServerPointSource(serverKey.ToString());
+                // Get the point source information of the current group(area)
+                PointSourceDataModel availablePointSource = await _tagCreatorStore.GetServerPointSource(serverKey.ToString());
                 if (availablePointSource != null)
                 {
                     pointSources.Add(serverKey, availablePointSource);
                     _areasPointSources.Add(availablePointSource);
                 }
             }
+
+            // Map and return the tags record to map definition format
             return tags.MapToPIPointDefinition(withDescriptor: true, pointSourceDefinitions: pointSources);
         }
-
-        
-
-        //public async Task<bool> TestAsync()
-        //{
-        //    try
-        //    {
-        //        var tags = await _tagCreatorStore.GetTagsForCreation();
-        //        if (tags != null && tags.Count() > 0)
-        //        {
-        //            PIServer piServer = PIAFUtils.GetPIServer(_eccPIServerName);
-        //            //Collect all the tags basic attributes required for insertion
-        //            var _pointsDefinitions = tags.MapToPIPointDefinition(withDescriptor: true);
-        //            //TODO: to be implemented -- Add the pointsrouce, location1 and location4 attributes gotten from ECCPI_POINT_SOURCES table (search for point source having the least number of tags)
-        //            //
-        //            try
-        //            {
-        //                tags = tags.DistinctBy(t => t.ECCPI_TAG_NAME);
-
-        //                //TODO: to be implemented -- Search for instrument tags if they exist in the ECCPI 
-
-        //                int _foundTagsNum = 0;
-        //                int _start = 0;
-        //                int _limit = 500;
-        //                //tags = tags.Take(10);
-        //                while (_start <= tags.Count())
-        //                {
-        //                    var _iTags = tags.Skip(_start).Take(_limit);
-
-        //                    IList<IEnumerable<PIPointQuery>> _queries = new List<IEnumerable<PIPointQuery>>();
-        //                    foreach (var tag in _iTags)
-        //                    {
-        //                        List<PIPointQuery> _query = new List<PIPointQuery>();
-        //                        _query.Add(new PIPointQuery(PICommonPointAttributes.InstrumentTag, OSIsoft.AF.Search.AFSearchOperator.Equal, tag.AREA_PI_TAG_NAME));
-        //                        _queries.Add(_query);
-        //                    }
-
-        //                    var findExistingTags = await PIPoint.FindPIPointsAsync(piServer, queries: _queries, attributeNames: new List<string>() { PICommonPointAttributes.InstrumentTag });
-        //                    _foundTagsNum += findExistingTags.Count();
-        //                    _start += _limit;
-        //                }
-        //                //foreach (var tag in tags.Take(1000))
-        //                //{
-        //                //    if (string.IsNullOrEmpty(_query))
-        //                //        _query = string.Format("{0}:={1}", PICommonPointAttributes.InstrumentTag, tag.AREA_PI_TAG_NAME);
-        //                //    else
-        //                //        _query += string.Format(" OR {0}:={1}", PICommonPointAttributes.InstrumentTag, tag.AREA_PI_TAG_NAME);
-        //                //}
-
-
-        //                //foreach (var res in queryResult)
-        //                //{
-        //                //    var dd = res;
-        //                //}
-        //                //Insert the tags in bulk
-        //                //AFListResults<string, PIPoint> _insertResult = piServer.CreatePIPoints(_pointsDefinitions);
-
-        //                // TODO: Update the duplicates occurs before the insertion
-
-        //                //Receive results, and update the status of each inserted tag
-        //                var ccc = _foundTagsNum;
-        //            }
-        //            catch (Exception e)
-        //            {
-        //                Logger.Error("ECCPITagCreator", e);
-        //            }
-        //        }
-        //        return true;
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Logger.Error("ECCPITagCreator", e);
-        //        return false;
-        //    }
-        //}
     }
 }
