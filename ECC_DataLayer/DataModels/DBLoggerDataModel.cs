ffa0bd425f38614740fd584430d8558b470d3e02
Mostafa Darwish <mdarwish@dollargeneral.com>
2021-10-27 11:10:24 +0300

    Add new DBLogger classes

---
+++ ECC_DataLayer/DataModels/DBLoggerDataModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ECC_DataLayer.DataModels
+{
+    public class DbLoggerDetailsDataModel
+    {
+        public int EASL_NUM { get; set; }
+        public long EAWFT_NUM { get; set; }
+        public string ECCPI_TAG_NAME { get; set; }
+        public string AREA_PI_TAG_NAME { get; set; }
+        public string SRC_PI_SERVER_CD { get; set; }
+        public int EASR_NUM { get; set; }
+        public string SVC_NAME { get; set; }
+        public string SVC_MSG { get; set; }
+        public string SVC_MSG_TYP { get; set; }
+        public string SVC_MSG_SEVIRITY { get; set; }
+        public int AREA_POINT_ID { get; set; }
+        public string SVC_PROPOSED_REMEDY { get; set; }
+    }
+
+    public class DbLoggerDataModel
+    {
+        public DbLoggerDataModel()
+        {
+            REMARKS = string.Empty;
+        }
+        public int EASR_NUM { get; set; }
+        public string SVC_NAME { get; set; }
+        public DateTime SVC_START_DT { get; set; }
+        public DateTime SVC_END_DT { get; set; }
+        public string REMARKS { get; set; }
+        public string SVC_STATUS { get; set; }
+    }
+
+    public class Severity
+    {
+        public static string Critical = "Critical";
+        public static string Exception = "Exception";
+        public static string Error = "Error";
+        public static string Warning = "Warning";
+        public static string Information = "Information";
+        public static string Debug = "Debug";
+    }
+
+    public class svcType
+    {
+        public static string Tag = "Tag";
+        public static string Template = "Template";
+        public static string Attribute = "Attribute";
+    }
+    public class Status
+    {
+        public static string Running = "Running";
+        public static string Succeed = "Succeed";
+        public static string Fail = "Fail";
+
+    }
+}
---
+++ ECC_DataLayer/Stores/DbLoggerStore.cs
@@ -0,0 +1,90 @@
+﻿using ECC_DataLayer.Helpers;
+using ECC_DataLayer.Repositories;
+using ECC_DataLayer.Stores.Abstract;
+using System;
+using System.Threading.Tasks;
+using ECC_DataLayer.DataModels;
+
+namespace ECC_DataLayer.Stores
+{
+    public class DbLoggerStore : DataStore
+    {
+        public int ExecuteDbLogStartServiceQuery(string serviceName, string status, string remarks)
+        {
+            try
+            {
+                string query = string.Format(QueryReader.ReadQuery("DbLogStartServiceQuery"),
+                    ResolveQueryParam(serviceName),
+                    ResolveQueryParam(status),
+                    ResolveQueryParam(remarks));
+
+                var id = _repo.InsertAndReturnId(query);
+                return id;
+            }
+            catch (Exception e)
+            {
+                Logger.Error("DbLoggerStore.ExecuteDbLogStartServiceQuery", e);
+                return 0;
+            }
+        }
+
+        public async Task<int> ExecuteDbLogEndServiceQuery(int id, string status, string remarks)
+        {
+            try
+            {
+                string query = string.Format(QueryReader.ReadQuery("DbLogEndServiceQuery"),
+                    ResolveQueryParam(status),
+                    ResolveQueryParam(SafeLogMessage(remarks)),
+                    ResolveQueryParam(id));
+                await _repo.ExecuteScalarAsync(query, new { });
+                await Commit();
+                return 1;
+            }
+            catch (Exception e)
+            {
+                Logger.Error("DbLoggerStore.ExecuteDbLogEndServiceQuery", e);
+                return 0;
+            }
+        }
+
+        public async Task<int> ExecuteAddDbLogDetails(DbLoggerDetailsDataModel dbLoggerDetailsDataModel)
+        {
+            try
+            {
+                string query = string.Format(QueryReader.ReadQuery("AddDbLogDetails"),
+                    ResolveQueryParam(dbLoggerDetailsDataModel.EAWFT_NUM),
+                    ResolveQueryParam(dbLoggerDetailsDataModel.ECCPI_TAG_NAME),
+                    ResolveQueryParam(dbLoggerDetailsDataModel.AREA_PI_TAG_NAME),
+                    ResolveQueryParam(dbLoggerDetailsDataModel.SRC_PI_SERVER_CD),
+                    ResolveQueryParam(dbLoggerDetailsDataModel.EASR_NUM),
+                    ResolveQueryParam(dbLoggerDetailsDataModel.SVC_NAME),
+                    ResolveQueryParam(dbLoggerDetailsDataModel.SVC_MSG),
+                    ResolveQueryParam(dbLoggerDetailsDataModel.SVC_MSG_TYP),
+                    ResolveQueryParam(dbLoggerDetailsDataModel.SVC_MSG_SEVIRITY),
+                    ResolveQueryParam(dbLoggerDetailsDataModel.AREA_POINT_ID),
+                    ResolveQueryParam(dbLoggerDetailsDataModel.SVC_PROPOSED_REMEDY));
+                await _repo.ExecuteScalarAsync(query, new { });
+                await Commit();
+                return 1;
+            }
+            catch (Exception e)
+            {
+                Logger.Error("DbLoggerStore.ExecuteAddDbLogDetails", e);
+                return 0;
+            }
+        }
+        private static string SafeLogMessage(string message)
+        {
+            if (string.IsNullOrEmpty(message)) return string.Empty;
+
+            message = message.Replace('\t', ' ');
+            message = message.Replace(Environment.NewLine, " --> ");
+            message = message.Replace("\n", " --> ");
+            message = message.Replace('\'', ' ');
+            message = message.Replace('"', ' ');
+            if (message.Length > 1000)
+                message = message.Substring(0, 1000);
+            return message;
+        }
+    }
+}
